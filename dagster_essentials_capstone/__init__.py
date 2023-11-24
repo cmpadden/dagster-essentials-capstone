f78fdd9c19ed6bffcb486d5314805e19f7f7bcea
Colton Padden <colton.padden@fastmail.com>
2023-11-24 13:45:11 -0500

    asset to export resulting film details to json

--- dagster_essentials_capstone/__init__.py
+++ dagster_essentials_capstone/__init__.py
@@ -1,13 +1,14 @@
 from dagster import Definitions, load_assets_from_modules
 
-from .assets import letterboxd, metrics, opensubtitles
+from .assets import export, letterboxd, metrics, opensubtitles
 from .resources import database_resource
 
 letterboxd_assets = load_assets_from_modules([letterboxd])
 opensubtitles_assets = load_assets_from_modules([opensubtitles])
 metric_assets = load_assets_from_modules([metrics])
+export_assets = load_assets_from_modules([export])
 
 defs = Definitions(
-    assets=[*letterboxd_assets, *opensubtitles_assets, *metric_assets],
+    assets=[*letterboxd_assets, *opensubtitles_assets, *metric_assets, *export_assets],
     resources={"database": database_resource},
 )
---
+++ dagster_essentials_capstone/assets/export.py
@@ -0,0 +1,37 @@
+""" Exports used in website.
+"""
+from dagster import asset
+from dagster_duckdb import DuckDBResource
+
+from dagster_essentials_capstone.constants import (
+    DUCKDB_TABLE_LETTERBOXD_FILMS_DETAILS, DUCKDB_TABLE_OPENAI_SUMMARY)
+
+
+@asset(deps=["openai_film_summary", "letterboxd_poster_image"])
+def film_details_json_dump(database: DuckDBResource):
+    with database.get_connection() as conn:
+        conn.execute(
+            f"""
+            copy (
+                select
+                  d.film_slug,
+                  d.title,
+                  d.synopsis,
+                  d.description,
+                  d.release_date,
+                  d.external_links,
+                  d.genre_links,
+                  d.genre_names,
+                  d.cast_details,
+                  d.stats,
+                  s.openai_total_tokens,
+                  s.openai_prompt_tokens,
+                  s.openai_completion_tokens,
+                  s.openai_total_cost,
+                  s.summary
+                from {DUCKDB_TABLE_LETTERBOXD_FILMS_DETAILS} as d
+                left join {DUCKDB_TABLE_OPENAI_SUMMARY} as s
+                  on d.film_slug = s.film_slug
+            ) to 'data/production/film_details.json';
+            """
+        )
